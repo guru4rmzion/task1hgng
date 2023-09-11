c250f6a829d7b759665c36bd0830cad035fd820f
guru4rmzion <guru4rmzion@gmail.com>
2023-09-11 12:28:33 +0100

    updated my slack details

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 const express = require('express');
-   const app = express();
-   const port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-   // Middleware to parse JSON request bodies
-   app.use(express.json());
+// Middleware to parse JSON request bodies
+app.use(express.json());
 
-   // Define your endpoint route
-   app.get('/api', (req, res) => {
-     // Get query parameters
-     const slackName = req.query.slack_name;
-     const track = req.query.track;
+// Define your endpoint route
+app.get('/api', (req, res) => {
+    // Get query parameters
+    const slackName = req.query.slack_name || 'Alexander Oseji'; // Use your provided Slack name as a default
+    const track = req.query.track || 'Backend'; // Use 'Backend' as a default track
 
-     // Get the current day of the week
-     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-     const currentDay = daysOfWeek[new Date().getDay()];
+    // Get the current day of the week
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = daysOfWeek[new Date().getDay()];
 
-     // Get the current UTC time with a +/-2 minute window
-     const now = new Date();
-     const utcTime = new Date(now.getTime() + now.getTimezoneOffset() * 60000).toISOString();
+    // Get the current UTC time with a +/-2 minute window
+    const now = new Date();
+    const utcTime = new Date(now.getTime() + now.getTimezoneOffset() * 60000).toISOString();
 
-     // Define your response JSON
-     const response = {
-       slack_name: slackName,
-       current_day: currentDay,
-       utc_time: utcTime,
-       track: track,
-       github_file_url: 'https://github.com/guru4rmzion/task1hgng/blob/main/app.js',
-       github_repo_url: 'https://github.com/guru4rmzion/task1hgng.git',
-       status_code: 200,
-     };
+    // Define your response JSON
+    const response = {
+        slack_name: slackName,
+        current_day: currentDay,
+        utc_time: utcTime,
+        track: track,
+        github_file_url: 'https://github.com/username/repo/blob/main/file_name.ext', // Update with your GitHub details
+        github_repo_url: 'https://github.com/username/repo', // Update with your GitHub details
+        status_code: 200,
+    };
 
-     // Send the JSON response
-     res.json(response);
-   });
+    // Send the JSON response
+    res.json(response);
+});
 
-   // Start the server
-   app.listen(port, () => {
-     console.log(`Server is running on port ${port}`);
-   });
-   
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
